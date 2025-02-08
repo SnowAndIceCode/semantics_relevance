27aa524660a92ce3abcd86df7ce2132679cd81b5
王雪冰 <wangxuebing@58.com>
2025-02-08 17:41:44 +0800

    add pairwise

--- criteria.py
+++ criteria.py
@@ -6,32 +6,55 @@
 # @Description: there is code description.
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-class MultiTaskLossWithMRL(nn.Module):
+class MultiTaskLossWithRankNet(nn.Module):
     '''
     mlm+pointwise+pairwise
     {query,doc_neg,doc_posi}
 
     '''
-    def __init__(self,margin=1.0,train_type='post_pretrain'):
+    def __init__(self,train_type='post_pretrain'):
         super().__init__()
-        self.criterion = nn.CrossEntropyLoss()
+        self.ce_loss = nn.CrossEntropyLoss()
         self.bce_loss = nn.BCEWithLogitsLoss() # pointwise
-        self.mrl_loss = nn.MarginRankingLoss(margin=margin)
         self.train_type = train_type
 
-    def forward(self,prediction_scores,labels,classification_logits,next_sentence_labels):
+    def cul_rankNet(self,posi_score,neg_score):
+        y_loss = -F.logsigmoid(posi_score - neg_score)
+        return y_loss.mean()
+
+    def forward(self,neg_prediction_scores=None,neg_labels=None,posi_prediction_scores=None,posi_labels=None,neg_classification_logits=None,
+                neg_nsp_label=None,posi_classification_logits=None,posi_nsp_label=None):
         # pred:[batch*seq,vocab_size],
-        mlm_loss = self.criterion(prediction_scores.view(-1, 21128), labels.view(-1))
-        pointwise_loss = self.bce_loss(classification_logits,next_sentence_labels)
-        pairwise_loss = self.criterion()
-        losses = mlm_loss + pointwise_loss + pairwise_loss
-        return losses
+
+        if self.train_type == 'post_pretrain':
+            neg_mlm_loss = self.criterion(neg_prediction_scores.view(-1, 21128), neg_labels.view(-1))
+            posi_mlm_loss = self.criterion(posi_prediction_scores.view(-1, 21128), posi_labels.view(-1))
+
+            neg_pointwise_loss = self.bce_loss(neg_classification_logits, neg_nsp_label)
+            posi_pointwise_loss = self.bce_loss(posi_classification_logits, posi_nsp_label)
+
+            pairwise_loss = self.cul_rankNet(posi_classification_logits,neg_classification_logits)
+
+            losses = neg_mlm_loss + posi_mlm_loss + neg_pointwise_loss + posi_pointwise_loss + pairwise_loss
+            return {'total_loss': losses, 'mlm_loss': neg_mlm_loss + posi_mlm_loss, "pointwise_loss": neg_pointwise_loss + posi_pointwise_loss, "pairwise_loss": pairwise_loss}
+
+        elif self.train_type == 'finetune':
+            neg_pointwise_loss = self.bce_loss(neg_classification_logits, neg_nsp_label)
+            posi_pointwise_loss = self.bce_loss(posi_classification_logits, posi_nsp_label)
+
+            pairwise_loss = self.cul_rankNet(posi_classification_logits, neg_classification_logits)
+
+            losses = neg_pointwise_loss + posi_pointwise_loss + pairwise_loss
+            return {'total_loss': losses, 'mlm_loss': 0.0, "pointwise_loss":neg_pointwise_loss+posi_pointwise_loss,"pairwise_loss":pairwise_loss}
+        else:
+            raise ValueError('train_type must be post_pretrain or finetune')
 
 
 class DoubleLoss(nn.Module):
     '''
-    mlm+pointwise+pairwise
+    mlm+pointwise
     '''
     def __init__(self,train_type='post_pretrain'):
         super().__init__()
@@ -46,10 +69,12 @@ class DoubleLoss(nn.Module):
             pointwise_loss = self.bce_loss(classification_logits,next_sentence_labels)
             losses = mlm_loss+pointwise_loss
             return {'total_loss':losses,'mlm_loss':mlm_loss,"pointwise_loss":pointwise_loss}
+
         elif self.train_type =='finetune':
             pointwise_loss = self.bce_loss(classification_logits, next_sentence_labels)
             return {'total_loss': pointwise_loss, 'mlm_loss': 0.0, "pointwise_loss": pointwise_loss}
         else:
             raise ValueError('train_type must be post_pretrain or finetune')
+
 if __name__ == '__main__':
     print('保持好心情！ ')
